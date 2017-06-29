a9639165dcb19fb981520963a59ac0a0a395ec02
NarayanChakraborty <narayanchakraborty1993@gmail.com>
2017-06-30 00:58:48 +0600

    Development of Creative Digital Template

--- js/main.js
+++ js/main.js
@@ -1,1 +1,32 @@
 
+ $(document).ready(function(){
+    
+		
+        //Smoth Scroll
+         jQuery(function() {
+            jQuery('a[href*="#"]:not([href="#"])').on('click',function(e) {
+                var headheight= jQuery("header").height();
+              if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                var target = jQuery(this.hash);
+                target = target.length ? target : jQuery('[name=' + this.hash.slice(1) +']');
+                if (target.length) {
+                  jQuery('html, body').animate({
+                    scrollTop: target.offset().top-100
+                  }, 1000);
+                  return false;
+                }
+              }
+            });
+          });
+         
+     
+                  $('.owl-carousel').owlCarousel({
+            loop: true,
+            margin: 10,
+            nav: true,
+            navText: ["<i class='fa fa-chevron-left'></i>", "<i class='fa fa-chevron-right'></i>"],
+                items:4,
+        });
+
+    })
+
